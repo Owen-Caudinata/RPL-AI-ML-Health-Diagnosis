a4843a580f8002481e33cabb46d77c24478ed806
Owen-Caudinata <owencau8@gmail.com>
2024-03-23 13:49:16 +0700

    RHD-19 update EHR

--- backend/express/routes/EHRRoutes.js
+++ backend/express/routes/EHRRoutes.js
@@ -1,18 +1,43 @@
-import { Router } from 'express';
-import { PrismaClient } from '@prisma/client';
+import { Router } from "express";
+import { PrismaClient } from "@prisma/client";
 
 const router = Router();
 const prisma = new PrismaClient();
 
 router.get("/get", async (req, res) => {
-    try {
-        const ehrRecords = await prisma.electronicHealthRecord.findMany();
+  try {
+    const ehrRecords = await prisma.electronicHealthRecord.findMany();
 
-        res.status(200).json(ehrRecords);
-    } catch (error) {
-        console.error("Error fetching EHR records:", error);
-        res.status(500).send("Internal Server Error");
-    }
+    res.status(200).json(ehrRecords);
+  } catch (error) {
+    console.error("Error fetching EHR records:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+router.post("/create", async (req, res) => {
+  try {
+    // Extract necessary data from the request body
+    const { id, createdAt, updatedAt, title, content, published, user, userId } = req.body;
+
+    const createEHR = await prisma.electronicHealthRecord.create({
+      data: {
+        id: id,
+        createdAt: createdAt,
+        updatedAt: updatedAt,
+        title: title,
+        content: content,
+        published: published,
+        user: user,
+        userId: userId,
+      },
+    });
+
+    res.status(201).json(createEHR);
+  } catch (error) {
+    console.error("Error creating EHR record:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 export default router;
