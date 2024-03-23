cd9b24620845df3378fdff9afdc04eb3451a339e
Owen-Caudinata <owencau8@gmail.com>
2024-03-23 14:10:51 +0700

    RHD-20 Delete EHR

--- backend/express/routes/EHRRoutes.js
+++ backend/express/routes/EHRRoutes.js
@@ -1,18 +1,65 @@
-import { Router } from 'express';
-import { PrismaClient } from '@prisma/client';
+import { Router } from "express";
+import { PrismaClient } from "@prisma/client";
 
 const router = Router();
 const prisma = new PrismaClient();
 
 router.get("/get", async (req, res) => {
-    try {
-        const ehrRecords = await prisma.electronicHealthRecord.findMany();
+  try {
+    const ehrRecords = await prisma.electronicHealthRecord.findMany();
 
-        res.status(200).json(ehrRecords);
-    } catch (error) {
-        console.error("Error fetching EHR records:", error);
-        res.status(500).send("Internal Server Error");
+    res.status(200).json(ehrRecords);
+  } catch (error) {
+    console.error("Error fetching EHR records:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+router.post("/create", async (req, res) => {
+  try {
+    const { title, content, published, user, userId } = req.body;
+
+    const createEHR = await prisma.electronicHealthRecord.create({
+      data: {
+        title: title,
+        content: content,
+        published: published,
+        userId: userId,
+      },
+    });
+
+    res.status(201).json(createEHR);
+  } catch (error) {
+    console.error("Error creating EHR record:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+router.delete("/delete/:id", async (req, res) => {
+  try {
+    const ehrId = parseInt(req.params.id);
+
+    const ehrRecord = await prisma.electronicHealthRecord.findUnique({
+      where: {
+        id: ehrId,
+      },
+    });
+
+    if (!ehrRecord) {
+      return res.status(404).send("EHR record with ID selected not found");
     }
+
+    await prisma.electronicHealthRecord.delete({
+      where: {
+        id: ehrId,
+      },
+    });
+
+    res.status(204).send();
+  } catch (error) {
+    console.error("Error deleting EHR record:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 export default router;
