1cb27144657401b19de271e94f942256649c0cdc
fauzan04 <fauzanvictor04@gmail.com>
2024-03-24 12:36:15 +0700

    RHD-24 user reminder delete

--- backend/express/routes/ReminderRoutes.js
+++ backend/express/routes/ReminderRoutes.js
@@ -10,7 +10,7 @@ router.get("/get", async (req, res) => {
 
     res.status(200).json(reminderRecords);
   } catch (error) {
-    console.error("Error fetching reminder:", error);
+    console.error("Error fetching Reminder Record:", error);
     res.status(500).send("Internal Server Error");
   }
 });
@@ -34,7 +34,7 @@ router.post("/create", async (req, res) => {
 
     res.status(201).json(createReminder);
   } catch (error) {
-    console.error("Error creating reminder:", error);
+    console.error("Error creating Reminder Record:", error);
     res.status(500).send("Internal Server Error");
   }
 });
@@ -51,7 +51,7 @@ router.put("/edit/:id", async (req, res) => {
     });
 
     if (!existingReminder) {
-      return res.status(404).send("Reminder from current ID not found");
+      return res.status(404).send("Reminder Record from current ID not found");
     }
 
     const editReminder = await prisma.reminderRecords.update({
@@ -71,34 +71,34 @@ router.put("/edit/:id", async (req, res) => {
 
     res.status(200).json(editReminder);
   } catch (error) {
-    console.error("Error updating Reminder:", error);
+    console.error("Error updating Reminder Record:", error);
     res.status(500).send("Internal Server Error");
   }
 });
 
 router.delete("/delete/:id", async (req, res) => {
   try {
-    const ehrId = parseInt(req.params.id);
+    const reminderId = parseInt(req.params.id);
 
-    const ehrRecord = await prisma.electronicHealthRecord.findUnique({
+    const reminderRecord = await prisma.reminderRecord.findUnique({
       where: {
-        id: ehrId,
+        id: reminderId,
       },
     });
 
-    if (!ehrRecord) {
-      return res.status(404).send("EHR record with ID selected not found");
+    if (!reminderRecord) {
+      return res.status(404).send("Reminder Record with ID selected not found");
     }
 
-    await prisma.electronicHealthRecord.delete({
+    await prisma.reminderRecord.delete({
       where: {
-        id: ehrId,
+        id: reminderId,
       },
     });
 
     res.status(204).send();
   } catch (error) {
-    console.error("Error deleting EHR record:", error);
+    console.error("Error deleting Reminder record:", error);
     res.status(500).send("Internal Server Error");
   }
 });
