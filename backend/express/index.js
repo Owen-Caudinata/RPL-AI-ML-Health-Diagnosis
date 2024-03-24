54d6b8201fc4aa44668a97d642f7d976be222401
RakhaYandra <rakhaputrapebriyandra272@gmail.com>
2024-03-24 12:50:06 +0700

    RHD-35 <User Send Feedback Create>

--- backend/express/index.js
+++ backend/express/index.js
@@ -3,6 +3,7 @@ import { PrismaClient } from "@prisma/client";
 import ehrRouter from "./routes/EHRRoutes.js";
 import dailyNewsLetterRouter from "./routes/DailyNewsLetterRoutes.js";
 import reminderRouter from "./routes/ReminderRoutes.js";
+import feedbackRouter from "./routes/FeedbackRoutes.js";
 
 const prisma = new PrismaClient();
 const app = express();
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use("/ehr", ehrRouter);
 app.use("/dailyNewsletter", dailyNewsLetterRouter);
 app.use("/reminder", reminderRouter);
+app.use("/feedback", feedbackRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
