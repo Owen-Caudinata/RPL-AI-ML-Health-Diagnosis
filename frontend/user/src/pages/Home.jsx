0fd4850cea807e0365ea63bde7e1e853407cfe1a
fauzan04 <fauzanvictor04@gmail.com>
2024-04-05 19:42:23 +0700

    RHD-24 modify text

--- frontend/user/src/pages/Home.jsx
+++ frontend/user/src/pages/Home.jsx
@@ -60,11 +60,11 @@ const Home = () => {
             Care: Easily obtain <br />
             accurate health results.
           </Heading>
-          <Text lineHeight="27px" letterSpacing="1px" mt={10} ml={{ base: 5, md: 10 }} color={theme.colors.cyan[700]} fontSize="md">
+          <Text lineHeight="25px" letterSpacing="0.5px" mt={10} ml={{ base: 5, md: 10 }} color={theme.colors.cyan[700]} fontSize="md">
             Our user-friendly platform enables you to manage your health more <br />
             easily, providing a convenient way to obtain the care you need. Take <br />
-            control of your health with ease and precision - because your well-being <br />
-            deserves the best.
+            control of your health with ease and precision - because your well- <br />
+            being deserves the best.
           </Text>
         </Box>
         <Box opacity={fadeIn ? 1 : 0} transition="opacity 0.5s ease-in-out" ml={{ md: 20 }}>
