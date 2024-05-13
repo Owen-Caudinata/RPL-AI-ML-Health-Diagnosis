24874597b792836c204cc902320ce28300bd0e66
ingeniom22 <jamesmichael0444@gmail.com>
2024-05-13 13:44:25 +0700

    add tests

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,8 +1,6 @@
 import unittest
 import os
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
 from dotenv import load_dotenv
 
 load_dotenv()
