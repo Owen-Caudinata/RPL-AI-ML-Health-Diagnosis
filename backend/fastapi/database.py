9c72cb692317ae2479c58d323dc7551f1148bfa3
ingeniom22 <jamesmichael0444@gmail.com>
2024-06-02 23:54:58 +0700

    fix fetal preds

--- backend/fastapi/database.py
+++ backend/fastapi/database.py
@@ -1,9 +1,16 @@
+import os
 import motor.motor_asyncio
+from dotenv import load_dotenv
 
-client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://localhost:27017/")
+load_dotenv()
+
+MONGO_URL = os.getenv("MONGO_URL")
+
+client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URL)
 db = client.get_database("rpl")
 
 alzheimer_preds = db.get_collection("alzheimer_preds")
 pneumonia_preds = db.get_collection("pneumonia_preds")
+fetal_preds = db.get_collection("fetal_preds")
 
 
